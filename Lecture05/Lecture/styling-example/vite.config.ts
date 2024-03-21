2ef6b913827ab8a880f166e4855c4567b8d5d8db
ArsenicRo <kamil.k.mat@gmail.com>
2024-03-21 17:55:05 +0100

    Add CSS SourceMap example

--- Lecture05/Lecture/styling-example/vite.config.ts
+++ Lecture05/Lecture/styling-example/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [["@emotion/babel-plugin", { sourceMap: true }]],
+      },
+    }),
+  ],
+});
