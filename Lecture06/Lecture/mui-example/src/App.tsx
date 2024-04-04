76ef80e458c3e34323f7b6cff084fa8f629fe977
ArsenicRo <kamil.k.mat@gmail.com>
2024-04-04 13:38:23 +0200

    After Lecture

--- Lecture06/Lecture/mui-example/src/App.tsx
+++ Lecture06/Lecture/mui-example/src/App.tsx
@@ -1,5 +1,113 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Button, Slider, SliderProps, alpha, styled } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import React from "react";
+
+// Po to żeby mieć reużywalny komponent
+
+interface StyledSliderProps extends SliderProps {
+  success?: boolean;
+}
+
+const SuccessSlider = styled(Slider, {
+  shouldForwardProp: (prop) => prop !== "success",
+})<StyledSliderProps>(({ success, theme }) => ({
+  width: 300,
+  ...(success && {
+    color: theme.palette.success.main,
+    "& .MuiSlider-thumb": {
+      "&:hover, &.Mui-focusVisible": {
+        boxShadow: `0px 0px 0px 8px ${alpha(theme.palette.success.main, 0.16)}`,
+      },
+      "&.Mui-active": {
+        boxShadow: `0px 0px 0px 14px ${alpha(
+          theme.palette.success.main,
+          0.16
+        )}`,
+      },
+    },
+  }),
+}));
+
+function DialogContentCustom() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <DialogContent>
+      <DialogContentText>Count: {count}</DialogContentText>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
+    </DialogContent>
+  );
+}
+
+function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContentCustom />
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
 function App() {
-  return <div>App</div>;
+  return (
+    <Box>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<DeleteIcon />}
+        sx={{
+          bgcolor: "red",
+          "&:hover": {
+            bgcolor: "blue",
+          },
+        }}
+      >
+        Contained
+      </Button>
+      <Button variant="contained" color="primary" startIcon={<DeleteIcon />}>
+        Contained
+      </Button>
+      <Button variant="contained" color="secondary" startIcon={<DeleteIcon />}>
+        Contained
+      </Button>
+
+      <Button variant="outlined">Outlined</Button>
+      <AlertDialog />
+      <SuccessSlider success />
+    </Box>
+  );
 }
 
 export default App;
--- Lecture06/Lecture/mui-example/src/main.tsx
+++ Lecture06/Lecture/mui-example/src/main.tsx
@@ -2,8 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import { ThemeProvider } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { green, lime } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+const f = (style: any) => {
+  if (style.variant === "contained" && style.color === "primary") {
+    return { fontSize: "2rem" };
+  }
+  return {};
+};
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: lime,
+    secondary: green,
+  },
+  components: {
+    // Name of the component
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === "contained" &&
+            ownerState.color === "primary" && {
+              fontSize: "2rem",
+            }),
+        }),
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
